4e4fa68f8fb32404539a76bfe317737ad6cadee0
artemmarkaryan <artemmarkaryan@yandex.ru>
2023-01-10 21:36:05 +0300

    add live route

--- cmd/main.go
+++ cmd/main.go
@@ -9,5 +9,8 @@ import (
 
 func main() {
 	log.Print("Serving...")
+	http.HandleFunc("/live", func(writer http.ResponseWriter, _ *http.Request) {
+		writer.WriteHeader(http.StatusTeapot)
+	})
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
