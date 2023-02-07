f7d577e27a8340b867a87951b7178f15571ebfea
artemmarkaryan <artemmarkaryan@yandex.ru>
2023-02-07 22:34:40 +0300

    remove dotenv file

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,11 @@ import (
 	"os"
 
 	_ "github.com/artemmarkaryan/exlex-backend/internal/migrations"
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/pressly/goose/v3"
 )
 
 func main() {
-	godotenv.Load(".env")
-	godotenv.Load("/etc/exlex/.env")
-
 	db, err := sql.Open("postgres", os.Getenv("PSQL_DSN"))
 	if err != nil {
 		log.Fatalln(err)
